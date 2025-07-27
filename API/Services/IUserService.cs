47d5d0399f1025bd84fad6572e68ed4752738b0e
Thien phuc <thienphuc484@gmail.com>
2025-07-27 13:13:24 +0700

    feat: add UserService

---
+++ API/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using API.Entity;
+
+namespace API.Services;
+
+public interface IUserService
+{
+    Task<List<User>> GetAllAsync();
+    Task<User?> GetByIdAsync(int id);
+    Task<User> AddAsync(User user);
+    Task<bool> UpdateAsync(int id, User user);
+    Task<bool> DeleteAsync(int id);
+}
