405f81709203048715e01db2ca5b1b6291ef2b0b
Thien phuc <thienphuc484@gmail.com>
2025-07-27 13:14:09 +0700

    feat: add UserController

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,20 @@
+using API.Data;
+using API.Services;
+using API.Services.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+builder.Services.AddScoped<IUserService, UserService>();
 
+builder.Services.AddDbContext<UserDbContext>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -13,7 +22,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello World!").WithName("GetWeatherForecast");
-
+app.MapControllers();
 app.Run();
