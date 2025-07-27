4e1fcefc98995428caa0b997b91a46c0d7999df5
Thien phuc <thienphuc484@gmail.com>
2025-07-27 13:11:20 +0700

    feat(Data): add UserDbContext

---
+++ API/Data/UserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    private string DbPath { get; }
+
+    public UserDbContext()
+    {
+        const Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "user.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    }
+}
