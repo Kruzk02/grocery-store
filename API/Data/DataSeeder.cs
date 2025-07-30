dfc93ac7d1530fca58070b2a8ec0298748223eac
Thien phuc <thienphuc484@gmail.com>
2025-07-30 09:29:54 +0700

    feat(Data): add DataSender for create and assign role

---
+++ API/Data/DataSeeder.cs
@@ -0,0 +1,43 @@
+﻿using API.Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Data;
+
+public class DataSeeder
+{
+    public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+    {
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        
+        string[] roleNames = [ "Admin", "User" ];
+
+        foreach (var role in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+        
+        const string adminEmail = "admin@example.com";
+        const string adminUsername = "adminUsername";
+        const string adminPassword = "Admin@123";
+        
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            adminUser = new ApplicationUser()
+            {
+                UserName = adminUsername,
+                Email = adminEmail,
+            };
+
+            var result = await userManager.CreateAsync(adminUser, adminPassword);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+}
