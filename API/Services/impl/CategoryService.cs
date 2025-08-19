c76c4ed495a0e0eb00f941f3f8d4bc1eb16e767d
Thien phuc <thienphuc484@gmail.com>
2025-08-19 10:22:13 +0700

    feat: add categoryService class and interface for handling retrieve categories

---
+++ API/Services/impl/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services.impl;
+
+/// <summary>
+/// Provides operations for retrieving categories.
+/// </summary>
+/// <remarks>
+/// This class interacts with database to performs retrieve operations related to categories.
+/// </remarks>
+/// <param name="ctx">the <see cref="ApplicationDbContext"/> used to access the database.</param>
+public class CategoryService(ApplicationDbContext ctx) : ICategoryService 
+{
+    /// <inheritdoc />
+    public async Task<ServiceResult<List<Category>>> FindAll()
+    {
+        var categories = await ctx.Categories.ToListAsync();
+        return categories.Count > 0 ? 
+            ServiceResult<List<Category>>.Ok(categories, "Categories retrieved successfully") : 
+            ServiceResult<List<Category>>.Failed("Failed to find all categories");
+    }
+}
---
+++ Tests/Services/CategoryServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entity;
+using API.Services.impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Services;
+
+[TestFixture]
+public class CategoryServiceTest
+{
+    private static ApplicationDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new ApplicationDbContext(options);
+    }
+    
+    [Test]
+    public async Task GetAllCategories_ShouldReturn_ListOfCategory()
+    {
+        var ctx = GetInMemoryDbContext();
+        var category = new Category { Id = 1, Name = "Fresh Produce", Description = "Fruits, vegetables, herbs" };
+        ctx.Categories.Add(category);
+        await ctx.SaveChangesAsync();
+        
+        var service = new CategoryService(ctx);
+
+        var result = service.FindAll();
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Result.Success, Is.True);
+            Assert.That(result.Result.Data, Is.Not.Null);
+            Assert.That(result.Result.Data.Count, Is.EqualTo(1));
+        });
+    }
+}
