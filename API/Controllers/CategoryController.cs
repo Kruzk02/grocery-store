861ea766d958db20f89b41fb8a0011e9b364bbd5
Thien phuc <thienphuc484@gmail.com>
2025-08-19 10:36:39 +0700

    feat: add CategoryController for handling request to categories

---
+++ API/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using API.Dtos;
+using API.Entity;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+/// <summary>
+/// Handles requests to categories.
+/// </summary>
+/// <remarks>
+/// This controller provided endpoints to retrieve categories.
+/// </remarks>
+/// <param name="service"></param>
+[ApiController, Route("[controller]"), Authorize]
+public class CategoryController(ICategoryService service) : ControllerBase
+{
+    /// <summary>
+    /// Retrieves all categories
+    /// </summary>
+    /// <returns>
+    /// A list of <see cref="Category"/> object wrapped in a
+    /// <see cref="ServiceResult{T}"/>.
+    /// </returns>
+    /// <response code="200">Returns the list of categories.</response>
+    /// <response code="500">If an unexpected error occurs.</response>
+    [HttpGet]
+    [ProducesResponseType(typeof(ServiceResult<List<Category>>), 200)]
+    [ProducesResponseType(500)]
+    public async Task<IActionResult> GetAll() => Ok(await service.FindAll());
+}
--- API/Program.cs
+++ API/Program.cs
@@ -13,8 +13,6 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -24,6 +22,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<IVerificationTokenService, VerificationTokenService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddIdentityCore<ApplicationUser>()
     .AddRoles<IdentityRole>()
