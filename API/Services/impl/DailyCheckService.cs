19ec5ef0e3f1342aa9b61dcdcdbafd2b5fdcb090
Thien phuc <thienphuc484@gmail.com>
2025-09-15 10:03:34 +0700

    feat(Services): add DailyCheckService

---
+++ API/Services/impl/DailyCheckService.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services.impl;
+
+public class DailyCheckService(ILogger<DailyCheckService> logger, IServiceScopeFactory scopeFactory) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("DailyCheckService is running.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var scope = scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            
+            var now = DateTime.Now;
+            var scheduledTime = DateTime.Today.AddHours(8);
+            
+            if (now > scheduledTime) {
+                scheduledTime = scheduledTime.AddDays(1);
+            }
+
+            var delay = scheduledTime - now;
+            logger.LogInformation("Next check at {time}", scheduledTime);
+            
+            await Task.Delay(delay, stoppingToken);
+            
+            var inventory = await dbContext.Inventories.Where(i => i.Quantity <= 10).FirstOrDefaultAsync(stoppingToken);
+            var adminUsers = await (from user in dbContext.Users
+                    join userRole in dbContext.UserRoles on user.Id equals userRole.UserId
+                    join role in dbContext.Roles on userRole.RoleId equals role.Id
+                    where role.Name == "Admin"
+                    select user)
+                .FirstOrDefaultAsync(cancellationToken: stoppingToken);
+
+            if (adminUsers?.Id == null) continue;
+            dbContext.Notifications.Add(new Notification
+            {
+                UserId = adminUsers.Id,
+                Message = "The product quality currently less than 10" + inventory?.Product,
+                Type = NotificationType.Info, IsRead = false, CreatedAt = DateTime.UtcNow
+            });
+            await dbContext.SaveChangesAsync(stoppingToken);
+        }
+    }
+}
