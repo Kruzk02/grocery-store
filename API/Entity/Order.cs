c851adaa5a913b5531b1008536b73a93beb62c76
Thien phuc <thienphuc484@gmail.com>
2025-09-19 09:09:30 +0700

    feat: add Customer and Order entity class

---
+++ API/Entity/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entity;
+
+public class Order
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public int CustomerId { get; set; }
+    [ForeignKey(nameof(CustomerId))]
+    public Customer customer { get; set; }
+    [Required, Range(0.01, double.MaxValue, ErrorMessage = "Total must be greater than 0")]
+    public double Total { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
