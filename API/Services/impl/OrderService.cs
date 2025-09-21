a870a9c940691509734563d5bec24a2c166f1ed2
Thien phuc <thienphuc484@gmail.com>
2025-09-21 12:44:44 +0700

    feat: add OrderService

---
+++ API/Services/impl/OrderService.cs
@@ -0,0 +1,83 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services.impl;
+
+public class OrderService(ApplicationDbContext ctx) : IOrderService
+{
+    public async Task<ServiceResult<Order>> Create(OrderDto orderDto)
+    {
+        var customer = await ctx.Customers.FindAsync(orderDto.CustomerId);
+        if (customer == null)
+        {
+            return ServiceResult<Order>.Failed("Customer not found");
+        }
+
+        var order = new Order
+        {
+            CustomerId = orderDto.CustomerId,
+            customer = customer
+        };
+        
+        var result = await ctx.Orders.AddAsync(order);
+        await ctx.SaveChangesAsync();
+        
+        return ServiceResult<Order>.Ok(result.Entity, "Order created successfully");
+    }
+
+    public async Task<ServiceResult> Update(int id, OrderDto orderDto)
+    {
+        var order = await ctx.Orders.FindAsync(id);
+        if (order == null)
+        {
+            return ServiceResult.Failed("Order not found");
+        }
+
+        if (orderDto.CustomerId != order.CustomerId && orderDto.CustomerId != 0)
+        {
+            var customer = await ctx.Customers.FindAsync(orderDto.CustomerId);
+            if (customer == null)
+            {
+                return ServiceResult.Failed("Customer not found");
+            }
+            
+            order.CustomerId = orderDto.CustomerId;
+            order.customer = customer;
+        }
+
+        await ctx.SaveChangesAsync();
+        
+        return ServiceResult.Ok("Order updated successfully");
+    }
+
+    public async Task<ServiceResult<Order>> FindById(int id)
+    {
+        var order = await ctx.Orders.FindAsync(id);
+        return order != null ? 
+            ServiceResult<Order>.Ok(order, "Order found") :
+            ServiceResult<Order>.Failed("Order not found");
+    }
+
+    public async Task<ServiceResult<List<Order>>> FindByCustomerId(int customerId)
+    {
+        var orders = await ctx.Orders.Where(o => o.CustomerId == customerId).ToListAsync();
+        return orders.Count > 0 ? 
+            ServiceResult<List<Order>>.Ok(orders, "Orders found") :
+            ServiceResult<List<Order>>.Failed("Order not found");
+    }
+
+    public async Task<ServiceResult> Delete(int id)
+    {
+        var order = await ctx.Orders.FindAsync(id);
+        if (order == null)
+        {
+            return ServiceResult.Failed("Order not found");
+        }
+
+        ctx.Orders.Remove(order);
+        await ctx.SaveChangesAsync();
+        return ServiceResult.Ok("Order deleted successfully");
+    }
+}
---
+++ Tests/Services/OrderServiceTest.cs
@@ -0,0 +1,116 @@
+﻿using API.Data;
+using API.Dtos;
+using API.Entity;
+using API.Services.impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Services;
+
+[TestFixture]
+public class OrderServiceTest
+{
+    private static ApplicationDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new ApplicationDbContext(options);
+    }
+
+    [Test]
+    public async Task Create()
+    {
+        var ctx = GetInMemoryDbContext();
+        var service = new OrderService(ctx);
+
+        var customer = new Customer() { Name = "Name", Email = "Email@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer);
+        await ctx.SaveChangesAsync();
+
+        var serviceResult = await service.Create(new OrderDto(customer.Id));
+        var result = serviceResult.Data;
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result!.Id, Is.GreaterThan(0));
+            Assert.That(result.CustomerId, Is.GreaterThan(0));
+        });
+    }
+    
+    [Test]
+    public async Task Update()
+    {
+        var ctx = GetInMemoryDbContext();
+        var service = new OrderService(ctx);
+
+        var customer1 = new Customer { Name = "Name", Email = "Email@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer1);
+        var customer2 = new Customer { Name = "Name123", Email = "Email123@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer1);
+        await ctx.SaveChangesAsync();
+
+        await service.Create(new OrderDto(customer1.Id));
+        var serviceResult = await service.Update(1, new OrderDto(customer2.Id));
+        
+        Assert.That(serviceResult.Success, Is.True);
+    }
+
+    [Test]
+    public async Task FindById()
+    {
+        var ctx = GetInMemoryDbContext();
+        var service = new OrderService(ctx);
+
+        var customer = new Customer { Name = "Name", Email = "Email@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer);
+        await ctx.SaveChangesAsync();
+        await service.Create(new OrderDto(customer.Id));
+        
+        var serviceResult = await service.FindById(1);
+        var result = serviceResult.Data;
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result!.Id, Is.GreaterThan(0));
+            Assert.That(result.CustomerId, Is.GreaterThan(0));
+        });
+    }
+
+    [Test]
+    public async Task FindByCustomerId()
+    {
+        var ctx = GetInMemoryDbContext();
+        var service = new OrderService(ctx);
+
+        var customer = new Customer { Name = "Name", Email = "Email@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer);
+        await ctx.SaveChangesAsync();
+        await service.Create(new OrderDto(customer.Id));
+        
+        var serviceResult = await service.FindByCustomerId(customer.Id);
+        var result = serviceResult.Data;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result![0].Id, Is.GreaterThan(0));
+            Assert.That(result[0].CustomerId, Is.GreaterThan(0));
+        });
+    }
+
+    [Test]
+    public async Task Delete()
+    {
+        var ctx = GetInMemoryDbContext();
+        var service = new OrderService(ctx);
+
+        var customer = new Customer { Name = "Name", Email = "Email@gmail.com", Phone = "84 123 456 78", Address = "2aad3"};
+        ctx.Customers.Add(customer);
+        await ctx.SaveChangesAsync();
+        await service.Create(new OrderDto(customer.Id));
+
+        var serviceResult = await service.Delete(1);
+        Assert.That(serviceResult.Success, Is.True);
+    }
+}
