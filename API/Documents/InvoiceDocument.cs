79842ad69dcfffeacf9eddfa52fae6de07378dbc
Thien phuc <thienphuc484@gmail.com>
2025-10-16 10:18:56 +0700

    Add basic invoice document

---
+++ API/Documents/InvoiceDocument.cs
@@ -0,0 +1,69 @@
+﻿using API.Entity;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace API.Documents;
+
+public class InvoiceDocument(Invoice invoice) : IDocument
+{
+    private Invoice Invoice { get; } = invoice;
+
+    public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+
+    public void Compose(IDocumentContainer container)
+    {
+        container
+            .Page(page =>
+            {
+                page.Margin(50);
+
+                page.Header().Element(ComposeHeader);
+                page.Content().Element(ComposeContent);
+
+                page.Footer().AlignCenter().Text(x =>
+                {
+                    x.CurrentPageNumber();
+                    x.Span(" / ");
+                    x.TotalPages();
+                });
+            });
+    }
+
+    private void ComposeHeader(IContainer container)
+    {
+        container.Row(row =>
+        {
+            row.RelativeItem().Column(column =>
+            {
+                column.Item()
+                    .Text($"#{Invoice.InvoiceNumber}")
+                    .FontSize(20).SemiBold().FontColor(Colors.Grey.Medium);
+
+                column.Item().Text(text =>
+                {
+                    text.Span("Issue: ").SemiBold();
+                    text.Span($"{Invoice.IssueDate:dd MMM yyyy}");
+                });
+
+                column.Item().Text(text =>
+                {
+                    text.Span("Due date: ").SemiBold();
+                    text.Span($"{Invoice.DueDate:dd MMM yyyy}");
+                });
+            });
+            row.ConstantItem(100).Height(50).Placeholder();
+        });
+    }
+
+    private void ComposeContent(IContainer container)
+    {
+        container
+            .PaddingVertical(40)
+            .Height(250)
+            .Background(Colors.Grey.Lighten3)
+            .AlignCenter()
+            .AlignMiddle()
+            .Text("Content").FontSize(16);
+    }
+}
