6aac44498300fa3621792268ce0b163796e189d5
Thien phuc <thienphuc484@gmail.com>
2025-12-16 09:36:17 +0700

    Add TestCaseSource to CategoryServiceTest

--- Tests/Services/CategoryServiceTest.cs
+++ Tests/Services/CategoryServiceTest.cs
@@ -20,11 +20,11 @@ public class CategoryServiceTest
     }
     
     [Test]
-    public async Task GetAllCategories_ShouldReturn_ListOfCategory()
+    [TestCaseSource(nameof(CreateCategories))]
+    public async Task GetAllCategories_ShouldReturn_ListOfCategory(List<Category> category)
     {
         var ctx = GetInMemoryDbContext();
-        var category = new Category { Id = 1, Name = "Fresh Produce", Description = "Fruits, vegetables, herbs" };
-        ctx.Categories.Add(category);
+        foreach (var item in category) ctx.Categories.Add(item);
         await ctx.SaveChangesAsync();
         
         var service = new CategoryService(ctx, new MemoryCache(new MemoryCacheOptions()));
@@ -33,7 +33,27 @@ public class CategoryServiceTest
         using (Assert.EnterMultipleScope())
         {
             Assert.That(result.Result, Is.Not.Null);
-            Assert.That(result.Result, Has.Count.EqualTo(1));
+            Assert.That(result.Result, Has.Count.EqualTo(13));
         }
+    }
+
+    private static IEnumerable<List<Category>> CreateCategories()
+    {
+        yield return
+        [
+            new Category { Id = 1, Name = "Fresh Produce", Description = "Fruits, vegetables, herbs" },
+            new Category { Id = 2, Name = "Dairy & Eggs", Description = "Milk, cheese, yogurt, eggs" },
+            new Category { Id = 3, Name = "Meat & Seafood", Description = "Fresh meat, poultry, seafood" },
+            new Category { Id = 4, Name = "Bakery", Description = "Bread and baked products" },
+            new Category { Id = 5, Name = "Pantry Staples", Description = "Rice, pasta, spices" },
+            new Category { Id = 6, Name = "Canned & Packaged", Description = "Canned goods, sauces, instant" },
+            new Category { Id = 7, Name = "Frozen Foods", Description = "Frozen meat, ice cream" },
+            new Category { Id = 8, Name = "Snacks & Confectionery", Description = "Chips, chocolates, biscuits" },
+            new Category { Id = 9, Name = "Beverages", Description = "Water, soda, tea, coffee" },
+            new Category { Id = 10, Name = "Household & Cleaning", Description = "Detergents, cleaning items" },
+            new Category { Id = 11, Name = "Personal Care & Health", Description = "Toiletries, health items" },
+            new Category { Id = 12, Name = "Baby & Kids", Description = "Baby food, diapers" },
+            new Category { Id = 13, Name = "Miscellaneous", Description = "Other / seasonal products" }
+        ];
     }
 }
