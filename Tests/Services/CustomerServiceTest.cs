4f0b29dd2ef514f0004dc1d8dcda417e6a076c18
Thien phuc <thienphuc484@gmail.com>
2025-12-16 09:38:31 +0700

    Add TestCaseSource to CustomerServiceTest

--- Tests/Services/CustomerServiceTest.cs
+++ Tests/Services/CustomerServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Dtos;
 using Application.Services.impl;
+using Domain.Entity;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
@@ -27,9 +28,10 @@ public class CustomerServiceTest
     }
 
     [Test]
-    public async Task GetAllCustomers()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task GetAllCustomers(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        await _customerService.Create(customerDto);
 
         var result = await _customerService.FindAll();
         
@@ -37,17 +39,14 @@ public class CustomerServiceTest
     }
     
     [Test]
-    public async Task Create()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task Create(CustomerDto customerDto)
     {
-        var result = await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var result = await _customerService.Create(customerDto);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(result!.Id, Is.Not.Zero);
-            Assert.That(result.Name, Is.EqualTo("Name"));
-            Assert.That(result.Email, Is.EqualTo("Email@gmail.com"));
-            Assert.That(result.Phone, Is.EqualTo("843806784"));
-            Assert.That(result.Address, Is.EqualTo("1b22"));
+            Assert.That(result.Id, Is.GreaterThan(0));
         }
     }
 
@@ -62,82 +61,96 @@ public class CustomerServiceTest
     }
 
     [Test]
-    public async Task FindById()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task FindById(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var customer = await _customerService.Create(customerDto);
 
-        var result = await _customerService.FindById(1);
+        var result = await _customerService.FindById(customer.Id);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(result!.Id, Is.Not.Zero);
-            Assert.That(result.Name, Is.EqualTo("Name"));
-            Assert.That(result.Email, Is.EqualTo("Email@gmail.com"));
-            Assert.That(result.Phone, Is.EqualTo("843806784"));
-            Assert.That(result.Address, Is.EqualTo("1b22"));
+            Assert.That(result.Id, Is.Not.Zero);
+            Assert.That(result.Name, Is.EqualTo(customer.Name));
+            Assert.That(result.Email, Is.EqualTo(customer.Email));
+            Assert.That(result.Phone, Is.EqualTo(customer.Phone));
+            Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
     }
     
     [Test]
-    public async Task FindByName()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task FindByName(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var customer = await _customerService.Create(customerDto);
 
-        var result = await _customerService.FindByName("Name");
+        var result = await _customerService.FindByName(customer.Name);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(result!.Id, Is.Not.Zero);
-            Assert.That(result.Name, Is.EqualTo("Name"));
-            Assert.That(result.Email, Is.EqualTo("Email@gmail.com"));
-            Assert.That(result.Phone, Is.EqualTo("843806784"));
-            Assert.That(result.Address, Is.EqualTo("1b22"));
+            Assert.That(result.Id, Is.Not.Zero);
+            Assert.That(result.Name, Is.EqualTo(customer.Name));
+            Assert.That(result.Email, Is.EqualTo(customer.Email));
+            Assert.That(result.Phone, Is.EqualTo(customer.Phone));
+            Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
     }
     
     [Test]
-    public async Task FindByEmail()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task FindByEmail(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var customer = await _customerService.Create(customerDto);
 
-        var result = await _customerService.FindByEmail("Email@gmail.com");
+        var result = await _customerService.FindByEmail(customer.Email);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(result!.Id, Is.Not.Zero);
-            Assert.That(result.Name, Is.EqualTo("Name"));
-            Assert.That(result.Email, Is.EqualTo("Email@gmail.com"));
-            Assert.That(result.Phone, Is.EqualTo("843806784"));
-            Assert.That(result.Address, Is.EqualTo("1b22"));
+            Assert.That(result.Id, Is.Not.Zero);
+            Assert.That(result.Name, Is.EqualTo(customer.Name));
+            Assert.That(result.Email, Is.EqualTo(customer.Email));
+            Assert.That(result.Phone, Is.EqualTo(customer.Phone));
+            Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
     }
     
     [Test]
-    public async Task FindByPhoneNumber()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task FindByPhoneNumber(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var customer = await _customerService.Create(customerDto);
 
-        var result = await _customerService.FindByPhoneNumber("843806784");
+        var result = await _customerService.FindByPhoneNumber(customer.Phone);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(result!.Id, Is.Not.Zero);
-            Assert.That(result.Name, Is.EqualTo("Name"));
-            Assert.That(result.Email, Is.EqualTo("Email@gmail.com"));
-            Assert.That(result.Phone, Is.EqualTo("843806784"));
-            Assert.That(result.Address, Is.EqualTo("1b22"));
+            Assert.That(result.Id, Is.Not.Zero);
+            Assert.That(result.Name, Is.EqualTo(customer.Name));
+            Assert.That(result.Email, Is.EqualTo(customer.Email));
+            Assert.That(result.Phone, Is.EqualTo(customer.Phone));
+            Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
     }
 
     [Test]
-    public async Task DeleteById()
+    [TestCaseSource(nameof(CreateCustomerDto))]
+    public async Task DeleteById(CustomerDto customerDto)
     {
-        await _customerService.Create(new CustomerDto("Name", "Email@gmail.com", "843806784", "1b22"));
+        var customer = await _customerService.Create(customerDto);
 
-        var result = await _customerService.DeleteById(1);
+        var result = await _customerService.DeleteById(customer.Id);
         
         Assert.That(result, Is.EqualTo("Customer deleted successfully"));
     }
+
+    private static IEnumerable<CustomerDto> CreateCustomerDto()
+    {
+        yield return new CustomerDto("Nam1e", "Email1@gmail.com", "843806784", "1b23");
+        yield return new CustomerDto("Nam2e", "Email2@gmail.com", "843806894", "1b24");
+        yield return new CustomerDto("Name3", "Email3@gmail.com", "843806554", "1b25");
+        yield return new CustomerDto("Nam5e", "Email4@gmail.com", "843806424", "1b26");
+        yield return new CustomerDto("Name6", "Email5@gmail.com", "843806324", "1b27");
+    } 
     
     private static ApplicationDbContext GetInMemoryDbContext()
     {
