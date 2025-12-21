b0502e808049dd4283aa3ee5fb117295733c9311
Thien phuc <thienphuc484@gmail.com>
2025-12-21 10:25:18 +0700

    Add fail case in CustomerServiceTest

--- Tests/Services/CustomerServiceTest.cs
+++ Tests/Services/CustomerServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using Application.Dtos;
 using Application.Services.impl;
-using Domain.Entity;
+using Domain.Exception;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
@@ -40,7 +40,7 @@ public class CustomerServiceTest
     
     [Test]
     [TestCaseSource(nameof(CreateCustomerDto))]
-    public async Task Create(CustomerDto customerDto)
+    public async Task CreateSuccess(CustomerDto customerDto)
     {
         var result = await _customerService.Create(customerDto);
 
@@ -48,6 +48,29 @@ public class CustomerServiceTest
         {
             Assert.That(result.Id, Is.GreaterThan(0));
         }
+    }
+    
+    [TestCase("", "e@mail.com", "123", "addr", "Name")]
+    [TestCase("Name", "", "123", "addr", "Email")]
+    [TestCase("Name", "e@mail.com", "", "addr", "Phone")]
+    [TestCase("Name", "e@mail.com", "123", "", "Address")]
+    public Task Create_ShouldThrowValidationException(
+        string name,
+        string email,
+        string phone,
+        string address,
+        string expectedKey)
+    {
+        var ex = Assert.ThrowsAsync<ValidationException>(
+            async () => await _customerService.Create(new CustomerDto(name, email, phone, address))
+        );
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(ex.Errors.ContainsKey(expectedKey), Is.True);
+            Assert.That(ex.Errors[expectedKey], Does.Not.Empty);
+        }
+        return Task.CompletedTask;
     }
 
     [Test]
@@ -58,6 +81,21 @@ public class CustomerServiceTest
         var result = await _customerService.Update(1, new CustomerDto("Name13", "Emai44l@gmail.com", "843806784", "1b22"));
         
         Assert.That(result, Is.EqualTo("Customer updated successfully"));
+    }
+
+    [Test]
+    [TestCase("", "e@mail.com", "123", "addr")]
+    [TestCase("Name", "", "123", "addr")]
+    [TestCase("Name", "e@mail.com", "", "addr")]
+    [TestCase("Name", "e@mail.com", "123", "")]
+    public Task Update_ShouldThrowNotFoundException(string name, string email, string phone, string address)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () =>
+            await _customerService.Update(1, new CustomerDto(name, email, phone, address)));
+        
+        Assert.That(ex.Message, Is.EqualTo($"Customer with id: 1 not found"));
+
+        return Task.CompletedTask;
     }
 
     [Test]
@@ -76,6 +114,19 @@ public class CustomerServiceTest
             Assert.That(result.Phone, Is.EqualTo(customer.Phone));
             Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
+    }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(4)]
+    public Task FindById_ShouldThrowNotFoundException(int id)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () => 
+            await _customerService.FindById(id));
+        Assert.That(ex.Message, Is.EqualTo($"Customer with id: {id} not found"));
+        return Task.CompletedTask;
     }
     
     [Test]
@@ -97,6 +148,19 @@ public class CustomerServiceTest
     }
     
     [Test]
+    [TestCase("we")]
+    [TestCase("zx")]
+    [TestCase("cd")]
+    [TestCase("ff")]
+    public Task FindByName_ShouldThrowNotFoundException(string name)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () => 
+            await _customerService.FindByName(name));
+        Assert.That(ex.Message, Is.EqualTo($"Customer with name: {name} not found"));
+        return Task.CompletedTask;
+    }
+    
+    [Test]
     [TestCaseSource(nameof(CreateCustomerDto))]
     public async Task FindByEmail(CustomerDto customerDto)
     {
@@ -112,6 +176,19 @@ public class CustomerServiceTest
             Assert.That(result.Phone, Is.EqualTo(customer.Phone));
             Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
+    }
+    
+    [Test]
+    [TestCase("we@gmail.com")]
+    [TestCase("zx@gmail.com")]
+    [TestCase("cd@gmail.com")]
+    [TestCase("ff@gmail.com")]
+    public Task FindByEmail_ShouldThrowNotFoundException(string email)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () => 
+            await _customerService.FindByEmail(email));
+        Assert.That(ex.Message, Is.EqualTo($"Customer with email: {email} not found"));
+        return Task.CompletedTask;
     }
     
     [Test]
@@ -131,6 +208,18 @@ public class CustomerServiceTest
             Assert.That(result.Address, Is.EqualTo(customer.Address));
         }
     }
+    
+    [Test]
+    [TestCase("123")]
+    [TestCase("456")]
+    [TestCase("789")]
+    public Task FindByPhone_ShouldThrowNotFoundException(string phone)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () => 
+            await _customerService.FindByPhoneNumber(phone));
+        Assert.That(ex.Message, Is.EqualTo($"Customer with number: {phone} not found"));
+        return Task.CompletedTask;
+    }
 
     [Test]
     [TestCaseSource(nameof(CreateCustomerDto))]
@@ -141,6 +230,19 @@ public class CustomerServiceTest
         var result = await _customerService.DeleteById(customer.Id);
         
         Assert.That(result, Is.EqualTo("Customer deleted successfully"));
+    }
+    
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(4)]
+    public Task DeleteById_ShouldThrowNotFoundException(int id)
+    {
+        var ex = Assert.ThrowsAsync<NotFoundException>(async () => 
+            await _customerService.DeleteById(id));
+        Assert.That(ex.Message, Is.EqualTo($"Customer with id: {id} not found"));
+        return Task.CompletedTask;
     }
 
     private static IEnumerable<CustomerDto> CreateCustomerDto()
