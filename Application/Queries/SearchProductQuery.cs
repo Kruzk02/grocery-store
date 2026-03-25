69dcfed203819c18bdaadbd994519af0cd3a1633
Thien phuc <thienphuc484@gmail.com>
2026-03-25 11:15:59 +0700

    Add searchProductQuery

---
+++ Application/Queries/SearchProductQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Queries;
+
+public record SearchProductQuery(string? Name, int Skip, ProductSortBy? SortBy, bool Ascending, int Take = 10)
+{ }
+
+public enum ProductSortBy
+{
+    Name,
+    Price,
+    CreatedAt
+}
