c84e06bc5d43aca45d645a5ae6b1cb86740cf497
Thien phuc <thienphuc484@gmail.com>
2026-04-02 11:57:37 +0700

    Add testcontainer psql

---
+++ Tests/Repository/PostgresFixture.cs
@@ -0,0 +1,28 @@
+﻿using Testcontainers.PostgreSql;
+
+namespace Tests.Repository;
+
+
+public class PostgresFixture
+{
+    private PostgreSqlContainer Container { get; set; } = null!;
+
+    public async Task StartAsync()
+    {
+        Container = new PostgreSqlBuilder("postgres:latest")
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+        await Container.StartAsync();
+    }
+
+    public async Task StopAsync()
+    {
+        await Container.DisposeAsync();
+    }
+
+    public string GetConnectionString()
+        => Container.GetConnectionString();
+}
