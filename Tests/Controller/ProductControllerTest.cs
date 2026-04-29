afba2340d56e3cd6797dd775f6deb8002d3c8f44
Thien phuc <thienphuc484@gmail.com>
2026-04-29 09:45:55 +0700

    Add more assert check

--- Tests/Controller/ProductControllerTest.cs
+++ Tests/Controller/ProductControllerTest.cs
@@ -1,6 +1,10 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 
+using Application.Common;
+
+using Domain.Entity;
+
 namespace Tests.Controller;
 
 [TestFixture]
@@ -17,11 +21,20 @@ public class ProductControllerTest : BaseControllerTest
         content.Add(new StringContent("1"), "Quantity");
 
         HttpResponseMessage response = await Client.PostAsync("/product", content);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created), "A request should succeed created product.");
+
+        var product = await response.Content.ReadFromJsonAsync<Product>();
+
+        Assert.That(product, Is.Not.Null, "Response body should not be null");
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+            Assert.That(product.Id, Is.GreaterThan(0));
+            Assert.That(product.Name, Is.EqualTo("Name"));
+            Assert.That(product.Description, Is.EqualTo("hello_world"));
+            Assert.That(product.Price, Is.EqualTo(12.99m));
+            Assert.That(product.CategoryId, Is.EqualTo(1));
+            Assert.That(product.Quantity, Is.EqualTo(1));
         }
     }
 
@@ -35,22 +48,25 @@ public class ProductControllerTest : BaseControllerTest
 
         HttpResponseMessage response = await Client.PostAsync("/product", content);
 
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.False);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
     }
 
     [Test, Order(1)]
     public async Task FindProductByIdReturnsOk()
     {
         HttpResponseMessage response = await Client.GetAsync("/product/1");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
+        var product = await response.Content.ReadFromJsonAsync<Product>();
+        Assert.That(product, Is.Not.Null, "Response body should not be null");
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(product.Id, Is.GreaterThan(0));
+            Assert.That(product.Name, Is.EqualTo("Name"));
+            Assert.That(product.Description, Is.EqualTo("hello_world"));
+            Assert.That(product.Price, Is.EqualTo(12.99m));
+            Assert.That(product.CategoryId, Is.EqualTo(1));
+            Assert.That(product.Quantity, Is.EqualTo(1));
         }
     }
 
@@ -58,25 +74,30 @@ public class ProductControllerTest : BaseControllerTest
     public async Task FindProductByIdReturnsNotFound()
     {
         HttpResponseMessage response = await Client.GetAsync("/product/1123");
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.False);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
-
 
     [Test, Order(3)]
     public async Task FindProductsReturnsOk()
     {
         HttpResponseMessage response =
             await Client.GetAsync("/product?Name=Name&Skip=0&SortBy=Price&Ascending=True&Take=10");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
+        var pageResult = await response.Content.ReadFromJsonAsync<PageResult<Product>>();
+        Assert.That(pageResult, Is.Not.Null);
+        Assert.That(pageResult.Total, Is.GreaterThan(0));
+
+        List<Product> products = pageResult.Data;
+        Assert.That(products, Is.Not.Null.And.Not.Empty, "List of product should not be null and empty");
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(products[0].Id, Is.GreaterThan(0));
+            Assert.That(products[0].Name, Is.EqualTo("Name"));
+            Assert.That(products[0].Description, Is.EqualTo("hello_world"));
+            Assert.That(products[0].Price, Is.EqualTo(12.99m));
+            Assert.That(products[0].CategoryId, Is.EqualTo(1));
+            Assert.That(products[0].Quantity, Is.EqualTo(1));
         }
     }
 
@@ -84,24 +105,14 @@ public class ProductControllerTest : BaseControllerTest
     public async Task GetImageReturnsNotFound()
     {
         HttpResponseMessage response = await Client.GetAsync("/product/nope.jpg");
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.False);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
 
     [Test, Order(5)]
     public async Task FindOrderItemByIdReturnsOk()
     {
         HttpResponseMessage response = await Client.GetAsync("/product/1/ordersItem");
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
 
     [Test, Order(6)]
@@ -116,10 +127,20 @@ public class ProductControllerTest : BaseControllerTest
 
         HttpResponseMessage response = await Client.PutAsync("/product/1", content);
 
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        var product = await response.Content.ReadFromJsonAsync<Product>();
+
+        Assert.That(product, Is.Not.Null, "Response body should not be null");
+
         using (Assert.EnterMultipleScope())
         {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(product.Id, Is.GreaterThan(0));
+            Assert.That(product.Name, Is.EqualTo("Name"));
+            Assert.That(product.Description, Is.EqualTo("hello_world"));
+            Assert.That(product.Price, Is.EqualTo(12.99m));
+            Assert.That(product.CategoryId, Is.EqualTo(1));
+            Assert.That(product.Quantity, Is.EqualTo(1));
         }
     }
 
@@ -133,34 +154,20 @@ public class ProductControllerTest : BaseControllerTest
 
         HttpResponseMessage response = await Client.PutAsync("/product/1", content);
 
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.False);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
     }
 
     [Test, Order(7)]
     public async Task DeleteByIdReturnsNoContent()
     {
         HttpResponseMessage response = await Client.DeleteAsync("/product/1");
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.True);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
     }
 
     [Test, Order(7)]
     public async Task DeleteByIdReturnsNotFound()
     {
         HttpResponseMessage response = await Client.DeleteAsync("/product/1234");
-
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(response.IsSuccessStatusCode, Is.False);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-        }
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
 }
