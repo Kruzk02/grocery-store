e6a5850efeaa05fcb3f8c9588c664b84e249fb77
Thien phuc <thienphuc484@gmail.com>
2026-05-01 10:40:37 +0700

    Add OrderControllerTest

---
+++ Tests/Controller/OrderControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+using Application.Dtos.Request;
+
+using Domain.Entity;
+
+namespace Tests.Controller;
+
+[TestFixture]
+public class OrderControllerTest : BaseControllerTest
+{
+    [Test, Order(0)]
+    public async Task CreateReturnCreated()
+    {
+
+        var content = JsonContent.Create(new OrderDto(2));
+        HttpResponseMessage response = await Client.PostAsync("/order", content);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+
+        var order = await response.Content.ReadFromJsonAsync<Order>();
+        Assert.That(order, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(order.Id, Is.GreaterThan(0));
+            Assert.That(order.CustomerId, Is.GreaterThan(0));
+        }
+    }
+
+    [Test, Order(0)]
+    public async Task CreateReturnNotFound()
+    {
+        var content = JsonContent.Create(new OrderDto(123));
+        HttpResponseMessage response = await Client.PostAsync("/order", content);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    [Test, Order(1)]
+    public async Task FindByIdReturnOk()
+    {
+        HttpResponseMessage response = await Client.GetAsync("/order/1");
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        var order = await response.Content.ReadFromJsonAsync<Order>();
+        Assert.That(order, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(order.Id, Is.GreaterThan(0));
+            Assert.That(order.CustomerId, Is.GreaterThan(0));
+        }
+    }
+
+    [Test, Order(1)]
+    public async Task FindByIdReturnNotFound()
+    {
+        HttpResponseMessage response = await Client.GetAsync("/order/123");
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    [Test, Order(2)]
+    public async Task FindOrderItemByIdReturnOk()
+    {
+        HttpResponseMessage response = await Client.GetAsync("/order/1/ordersItem");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test, Order(3)]
+    public async Task FindInvoiceByIdReturnNotFound()
+    {
+        HttpResponseMessage response = await Client.GetAsync("/order/1/invoice");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    [Test, Order(4)]
+    public async Task UpdateReturnOk()
+    {
+        var content = JsonContent.Create(new OrderDto(2));
+        HttpResponseMessage response = await Client.PutAsync("/order/1", content);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        var order = await response.Content.ReadFromJsonAsync<Order>();
+        Assert.That(order, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(order.Id, Is.GreaterThan(0));
+            Assert.That(order.CustomerId, Is.GreaterThan(0));
+        }
+    }
+
+    [Test, Order(5)]
+    public async Task DeleteReturnNoContent()
+    {
+        HttpResponseMessage response = await Client.DeleteAsync("/order/1");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+    }
+
+    [Test, Order(5)]
+    public async Task DeleteReturnNotFound()
+    {
+        HttpResponseMessage response = await Client.DeleteAsync("/order/123");
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+}
